109bee22ac4602421baf1060ecb0630ab9d0f2db
mrtmrngz <mert00marangoz@gmail.com>
2025-08-17 16:05:54 +0300

    last

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.get("/", async (req, res) => {
     res.send(`
         <h1>Anasayfa</h1>
+        <a href="/products">Ürünlere git</a>
     `)
 })
 
@@ -23,6 +24,39 @@ app.get("/second", async (req, res) => {
     res.send(`
         <h1>Main 2</h1>
     `)
+})
+
+app.get("/products", async (req, res) => {
+
+    const products = [
+        {
+            id: 1,
+            title: "PRD1",
+            price: 100
+        },
+        {
+            id: 2,
+            title: "PRD2",
+            price: 400
+        },
+        {
+            id: 3,
+            title: "PRD3",
+            price: 656
+        },
+        {
+            id: 5,
+            title: "PRD5",
+            price: 100323
+        },
+        {
+            id: 6,
+            title: "PRD10",
+            price: 1005466
+        }
+    ]
+
+    res.json(products)
 })
 
 
